5566b76528ed120d10db2f645bedaf6e88362447
AbhishekTmz <atmz1113@gmail.com>
2023-09-28 04:04:24 +0545

    New kotlin

---
+++ shared/src/commonMain/kotlin/com/example/kotlinmultiplatformsandbox/Greeting.kt
@@ -0,0 +1,18 @@
+package com.example.kotlinmultiplatformsandbox
+
+import daysUntilNewYear
+import kotlin.random.Random
+
+
+class Greeting {
+    private val platform: Platform = getPlatform()
+
+    fun greet(): List<String> = buildList {buildList {
+        add(if (Random.nextBoolean()) "Hi!" else "Hello!")
+        add("Guess what it is! > ${platform.name.reversed()}!")
+        add("\nThere are only ${daysUntilNewYear()} days left until New Year! 🎆")
+    }
+
+
+    }
+} 
